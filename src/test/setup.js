fc485b2f12ffcdf1d646ff70a212761aea883f48
jasrzhang <zhangshurui1989@gmail.com>
2024-03-06 10:57:24 +1100

    Added test code

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+describe('Environment Variables', () => {
+    it('should have REACT_APP_API_URL set', () => {
+      // Access the environment variable using process.env
+      const accessToken = process.env.ACCESS_TOKEN;
+  
+      // Expect apiUrl to be truthy, meaning it should not be undefined, null, or an empty string
+      expect(accessToken).toBeTruthy();
+    });
+  });
+  
